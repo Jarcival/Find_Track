2e80d59296661fb140b07730abfc8f77d726db89
Juan Daniel <arriagaverajuandaniel@gmail.com>
2026-05-28 17:27:51 -0600

    Retoques de back2

--- frontend/src/app/core/config/config.ts
+++ frontend/src/app/core/config/config.ts
@@ -1,3 +1,1 @@
-// Configuración de la API para FinTrack
-// Cambia este valor por la URL de producción de Railway cuando hagas el despliegue del Backend
-export const API_BASE_URL = 'http://localhost:3000/api';
+export const API_BASE_URL = 'https://findtrack-production.up.railway.app/api';
